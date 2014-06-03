2c2128328b0f27c316e9ee9d3631112208d2fc38
John-Huy Nguyen <johnhuyn@uci.edu>
2014-06-03 03:25:51 -0700

    test

---
+++ Assets/myGUIScript.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class myGUIScript : MonoBehaviour
+{
+	
+	public GUISkin myGUISkin;
+	public List<string> debugs;
+	private string group_name;
+	private string drawing_name;
+	public bool penDown = false;
+	public int stroke_name;
+	private Color penColor;
+	ColorPicker[] color_Picker;
+	private myNetworkHelperScript network_helper;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+		if (this.myGUISkin == null) {
+			addDebug ("Please assign a GUIskin on the editor!");
+			this.enabled = false;
+			return;
+		}
+		
+		network_helper = GameObject.Find ("myNetworkHelper").GetComponentInChildren<myNetworkHelperScript> ();
+		
+		group_name = "jkw";
+		drawing_name = "kappa";
+		stroke_name = 0;
+		
+		color_Picker = GameObject.FindObjectsOfType<ColorPicker> ();
+		foreach (ColorPicker elem in color_Picker) {
+			elem.useExternalDrawer = true;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	}
+	
+	void OnGUI ()
+	{
+		GUI.skin = this.myGUISkin;
+		group_name = GUI.TextField (new Rect (0, 0, Screen.width, 100), group_name);
+		drawing_name = GUI.TextField (new Rect (0, 100, Screen.width, 100), drawing_name);
+		
+		foreach (ColorPicker cp in color_Picker) {
+			cp._DrawGUI ();
+		}
+		
+		bool oldPenDown = penDown;
+		penDown = GUI.Toggle (new Rect (0, 300, Screen.width, 100), penDown, "Pen Down");
+		
+		if (GUI.changed) {
+			if (oldPenDown != penDown) {
+				if (penDown) {
+					stroke_name++;
+					network_helper.addStrokeColor ("" + stroke_name, penColor);
+					addDebug ("Pen on, Stroke name: " + stroke_name + ", Color: " + penColor);
+					oldPenDown = penDown;
+				} else {
+					addDebug ("Pen off.");
+				}
+			}
+		}
+		
+		
+		if (GUI.Button (new Rect (0, 400, Screen.width, 100), "Upload")) {
+			addDebug ("Uploading data with group name (" + group_name + ") and drawing name (" + drawing_name + ")");
+			network_helper.addStrokeColor ("Stroke Name", penColor);
+			network_helper.uploadPoints(group_name, drawing_name);
+			
+		}
+		int temp = 500;
+		foreach (string d in debugs) {
+			GUI.Label (new Rect (0, temp, Screen.width, 100), d);
+			temp += 100;
+		}
+		while (debugs.Count > 2) {
+			debugs.RemoveAt (0);
+		}
+	}
+	
+	void OnSetColor(Color color)
+	{
+		penColor = color;
+		addDebug ("New Color " + color);
+	}
+	
+	void OnGetColor (ColorPicker picker)
+	{
+		picker.NotifyColor (penColor);
+	}
+	
+	public void addDebug (string e)
+	{
+		debugs.Add (e);
+		Debug.Log (e);
+	}
+	
+}
---
+++ Assets/myLocationScript.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class myLocationScript : MonoBehaviour
+{
+	
+	public GUISkin myGUISkin;
+	private myNetworkHelperScript network_helper;
+	private myGUIScript local_gui;
+	private bool working = false;
+	
+	// Use this for initialization
+	IEnumerator Start ()
+	{
+		local_gui = GameObject.Find ("myGUI").GetComponentInChildren<myGUIScript> ();
+		network_helper = GameObject.Find ("myNetworkHelper").GetComponentInChildren<myNetworkHelperScript> ();
+		
+		if (this.myGUISkin == null) {  
+			local_gui.addDebug ("Please assign a GUIskin on the editor!");  
+			this.enabled = false;  
+			yield return false;  
+		} 
+		
+		if (!Input.location.isEnabledByUser) {
+			local_gui.addDebug ("User has not enabled Location");
+			yield return false;
+		}
+		
+		Input.location.Start (1f, 1f);
+		
+		// Wait until service initializes
+		int maxWait = 20;
+		while ((Input.location.status == LocationServiceStatus.Initializing) && (maxWait > 0)) {
+			yield return new WaitForSeconds (1);
+			maxWait--;
+		}
+		
+		// Service didn't initialize in 20 seconds
+		if (maxWait < 1) {
+			print ("Timed out");
+			yield return false;
+		}
+		
+		// Connection has failed
+		if (Input.location.status == LocationServiceStatus.Failed) {
+			local_gui.addDebug ("Unable to determine device location");
+			return false;
+		} else {
+			local_gui.addDebug ("Location: " + Input.location.lastData.latitude + " " +
+			                    Input.location.lastData.longitude + " " +
+			                    (long)Input.location.lastData.timestamp);
+			working = true;
+		}
+		
+		
+	}
+	
+	float lastLongitude = -1;
+	float lastLatitude = -1;
+	float lastAltitude = -1;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (working) {
+			
+			if (lastLongitude != Input.location.lastData.longitude || lastLatitude != Input.location.lastData.latitude || lastAltitude != Input.location.lastData.altitude) {
+				lastLongitude = Input.location.lastData.longitude;
+				lastLatitude = Input.location.lastData.latitude;
+				lastAltitude = Input.location.lastData.altitude;
+				
+				local_gui.addDebug ("Location Changed: " + lastLatitude + " " + lastLongitude + " " + lastAltitude);
+				
+			}
+			if (local_gui.penDown) {
+				//	local_gui.addDebug ("New Location Recorded: " + lastLatitude + " " +lastLongitude + " " + lastAltitude);
+				network_helper.addPoint ("" + local_gui.stroke_name, (long)Input.location.lastData.timestamp, lastLatitude, lastLongitude, lastAltitude);
+				
+			}
+		}
+	}
+	
+	void OnGUI ()
+	{
+		GUI.skin = this.myGUISkin;
+		
+		GUI.Label (new Rect (0, Screen.height - 150, Screen.width, 40), "Strokes/Points: " + network_helper.numStrokes + "/" + network_helper.numPoints);
+		GUI.Label (new Rect (0, Screen.height - 100, Screen.width, 40), "Current Location: " + lastLatitude + " " + lastLongitude + " " + lastAltitude);
+		
+	}
+}
